0c3d7b3c7f0053dacaf4aca6c56af79bb9006e03
Georges Abdel Junior Joseph <99396455+deldevlop@users.noreply.github.com>
2024-04-20 03:57:20 +0000

    Almost Done

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -5,8 +5,8 @@ function CardPhoto() {
         <div className="  text-center">
             <div className="row row-cols-1 row-cols-md-4 row-cols-lg-4 g-2 g-lg-4">
                 <div className="col">
-                    <div className="card">
-                        <img src="https://cdn.pixabay.com/photo/2016/09/29/13/08/planet-1702788_1280.jpg" className="card-img-top" alt="..." />
+                    <div className="card d-flex flex-column justify-content-between">
+                        <img src="https://cdn.pixabay.com/photo/2012/12/21/10/07/sneakers-71623_1280.jpg" className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">Card title</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -15,8 +15,8 @@ function CardPhoto() {
                     </div>
                 </div>
                 <div className="col">
-                    <div className="card">
-                        <img src="https://cdn.pixabay.com/photo/2018/06/09/17/25/trees-3464777_1280.jpg" className="card-img-top" alt="..." />
+                    <div className="card d-flex flex-column justify-content-between">
+                        <img src="https://cdn.pixabay.com/photo/2014/04/05/11/38/nike-316449_1280.jpg" className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">Card title</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -25,8 +25,8 @@ function CardPhoto() {
                     </div>
                 </div>
                 <div className="col">
-                    <div className="card">
-                        <img src="https://cdn.pixabay.com/photo/2015/07/27/20/16/book-863418_1280.jpg" className="card-img-top" alt="..." />
+                    <div className="card d-flex flex-column justify-content-between align-items-center">
+                        <img src="https://cdn.pixabay.com/photo/2018/09/27/10/46/sneakers-3706741_1280.jpg" className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">Card title</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -34,8 +34,8 @@ function CardPhoto() {
                         </div>
                     </div> </div>
                 <div className="col">
-                    <div className="card">
-                        <img src="https://cdn.pixabay.com/photo/2017/12/15/13/51/polynesia-3021072_1280.jpg" className="card-img-top" alt="..." />
+                    <div className="card d-flex flex-column justify-content-between">
+                        <img src="https://cdn.pixabay.com/photo/2020/05/04/07/15/nike-5128119_1280.jpg" className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">Card title</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import rigoImage from "../../img/rigo-baby.jpg";
+import nike_logo from "../../img/nike-logo-49324.png";
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark" data-bs-theme="dark">
             <div className="container">
-                <a className="navbar-brand" href="#">My React Landing Page</a>
+                <a className="navbar-brand fw-bold fst-italic" href=""><img src={nike_logo} style={{
+            width:'60px',
+            height:'40px',
+            backgroundColor: 'white',
+            margin:'auto',
+            objectFit:'cover',
+            //marginBottom:'10px'
+          }} /> NIKE</a>
+
                 <button className="navbar-toggler border border-success p-2 border-opacity-10" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon "></span>
                 </button>
