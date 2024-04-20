a6c58267a057b859820cff2e326147c189972cc7
Georges Abdel Junior Joseph <99396455+deldevlop@users.noreply.github.com>
2024-04-20 04:03:04 +0000

    Last Commit

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -8,7 +8,7 @@ function CardPhoto() {
                     <div className="card d-flex flex-column justify-content-between">
                         <img src="https://cdn.pixabay.com/photo/2012/12/21/10/07/sneakers-71623_1280.jpg" className="card-img-top" alt="..." />
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
+                            <h5 className="card-title">NIKE 1</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                             <a href="#" className="btn btn-primary">Find Out More!</a>
                         </div>
@@ -18,7 +18,7 @@ function CardPhoto() {
                     <div className="card d-flex flex-column justify-content-between">
                         <img src="https://cdn.pixabay.com/photo/2014/04/05/11/38/nike-316449_1280.jpg" className="card-img-top" alt="..." />
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
+                            <h5 className="card-title">NIKE 2</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                             <a href="#" className="btn btn-primary">Find Out More!</a>
                         </div>
@@ -28,7 +28,7 @@ function CardPhoto() {
                     <div className="card d-flex flex-column justify-content-between align-items-center">
                         <img src="https://cdn.pixabay.com/photo/2018/09/27/10/46/sneakers-3706741_1280.jpg" className="card-img-top" alt="..." />
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
+                            <h5 className="card-title">NIKE 3</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                             <a href="#" className="btn btn-primary">Find Out More!</a>
                         </div>
@@ -37,7 +37,7 @@ function CardPhoto() {
                     <div className="card d-flex flex-column justify-content-between">
                         <img src="https://cdn.pixabay.com/photo/2020/05/04/07/15/nike-5128119_1280.jpg" className="card-img-top" alt="..." />
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
+                            <h5 className="card-title">NIKE 4</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                             <a href="#" className="btn btn-primary">Find Out More!</a>
                         </div>
--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -3,8 +3,8 @@ import React from "react";
 
 function Footer(){
     return(
-        <div className="container-fluid bg-dark p-4 mt-5 mb-0 text-white text-center">
-            <i className="fa-regular fa-copyright"></i> DelDevlop
+        <div className="container-fluid bg-dark text-capitalize p-4 mt-5 mb-0 text-white text-center">
+        Just Share the <i>NIKE</i>  experience
         </div>
     );
 };
